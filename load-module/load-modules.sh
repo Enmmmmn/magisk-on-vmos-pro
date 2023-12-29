d60d5bb311d1de3829345634b6c648f7360b30e5
Enmmmmn <131130860+Enmmmmn@users.noreply.github.com>
2023-12-29 10:26:09 +0800

    fix bugs

--- load-module/load-modules.sh
+++ load-module/load-modules.sh
@@ -1,8 +1,6 @@
 #!/system/bin/sh
-#基础变量
-ROOTFS=$(cat /init_shell.sh | xargs -n 1 | grep "init" | sed -e "s|/init||" -e "s|user/0|data|")
 #加载列表
-list=$ROOTFS/data/adb/load-module/config/load-list
+list=/data/adb/load-module/config/load-list
 #清理列表
 sed -i "/^$/d" $list
 #删除文件
@@ -12,18 +10,18 @@ for module in $(cat $list); do
   #重启服务
   [ -z "$restart" ] && setprop ctl.stop zygote && setprop ctl.stop zygote_secondary && restart=true
   #执行文件
-  sh $ROOTFS/data/adb/load-module/backup/remove-$(basename $module).sh > /dev/null 2>&1
+  sh /data/adb/load-module/backup/remove-$(basename $module).sh > /dev/null 2>&1
   #删除文件
-  rm -f $ROOTFS/data/adb/load-module/backup/remove-$(basename $module).sh
+  rm -f /data/adb/load-module/backup/remove-$(basename $module).sh
   #修改文件
   sed -i "s|$module||" $list
 done
 #并行运行
 {
   #等待加载
-  while [ -z "$(cat $ROORFS/cache/magisk.log | grep "* Loading modules")" ]; do sleep 0.0; done
+  while [ -z "$(cat /cache/magisk.log | grep "* Loading modules")" ]; do sleep 0.0; done
   #加载文件
-  for module in $ROOTFS/data/adb/modules/*; do
+  for module in /data/adb/modules/*; do
     #检测状态
     [ -f "$module/disable" ] && continue
     #修改属性
@@ -45,16 +43,16 @@ done
         #备份文件
         if [ "$(cat "/system/$target")" ]; then
           #检查文件
-          [ -f "$ROOTFS/data/adb/load-module/backup/system/$target" ] && continue
+          [ -f "/data/adb/load-module/backup/system/$target" ] && continue
           #创建目录
-          mkdir -p "$ROOTFS/data/adb/load-module/backup/system/$(dirname "$target")" > /dev/null 2>&1
+          mkdir -p "/data/adb/load-module/backup/system/$(dirname "$target")" > /dev/null 2>&1
           #复制文件
-          cp -p "$ROOTFS/system/$target" "$ROOTFS/data/adb/load-module/backup/system/$target" || continue
+          cp -p "/system/$target" "/data/adb/load-module/backup/system/$target" || continue
           #修改文件
-          echo -e "cp -p $ROOTFS/data/adb/load-module/backup/system/$target $ROOTFS/system/$target\nrm $ROOTFS/data/adb/load-module/backup/system/$target" >> $ROOTFS/data/adb/load-module/backup/remove-$(basename $module).sh
+          echo -e "cp -p /data/adb/load-module/backup/system/$target /system/$target\nrm /data/adb/load-module/backup/system/$target" >> /data/adb/load-module/backup/remove-$(basename $module).sh
         else
           #修改文件
-          echo "rm -f $ROOTFS/system/$target" >> $ROOTFS/data/adb/load-module/backup/remove-$(basename $module).sh
+          echo "rm -f /system/$target" >> /data/adb/load-module/backup/remove-$(basename $module).sh
         fi
         #复制文件
         cp -fp "$module/system/$target" "/system/$target"
@@ -62,9 +60,9 @@ done
         #检查目录
         [ -d "/system/$target" ] && continue
         #创建目录
-        mkdir -m 755 "$ROOTFS/system/$target"
+        mkdir -m 755 "/system/$target"
         #修改文件
-        echo "rm -rf $ROOTFS/system/$target" >> $ROOTFS/data/adb/load-module/backup/remove-$(basename $module).sh
+        echo "rm -rf /system/$target" >> /data/adb/load-module/backup/remove-$(basename $module).sh
       fi
     done
     #修改文件
